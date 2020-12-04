9bc04a6e9f1e5c6c58e86ef3e9c1b72f5e876546
gitgundam <335968838@qq.com>
2020-12-04 17:24:22 +0800

    使用间谍函数监听测试button的click事件

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import Icon from './components/icon.vue';
 import ButtonGroup from './components/button-group.vue';
 import Input from './components/Input';
 import chai from 'chai'
-const expect = chai.expect
-
+import spies from 'chai-spies'
+chai.use(spies)
 Vue.component('h-button', Button);
 Vue.component('h-icon', Icon);
 Vue.component('h-button-group', ButtonGroup);
@@ -16,6 +16,7 @@ new Vue({
   render: h => h(App),
 }).$mount('#app');
 
+const expect = chai.expect
 
 {
   const Constructor = Vue.extend(Button)
@@ -28,6 +29,9 @@ new Vue({
   const useElement = button.$el.querySelector('use')
   const href = useElement.getAttribute('xlink:href')
   expect(href).to.equal('#i-setting')
+  button.$el.remove()
+  button.$destroy()
+
 }
 {
   const Constructor = Vue.extend(Button)
@@ -41,6 +45,9 @@ new Vue({
   const useElement = button.$el.querySelector('use')
   const href = useElement.getAttribute('xlink:href')
   expect(href).to.equal('#i-loading')
+  button.$el.remove()
+  button.$destroy()
+
 
 }
 
@@ -48,17 +55,32 @@ new Vue({
   const div = document.createElement('div')
   document.body.appendChild(div)
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData:{
       icon: 'setting'
     }
   })
-  button.$mount(div)
-  const svg = button.$el.querySelector('svg')
+  vm.$mount(div)
+  const svg = vm.$el.querySelector('svg')
   const order = window.getComputedStyle(svg).order
   expect(order).to.equal('1')
-  button.$el.remove()
-  button.$destroy()
-
+  vm.$el.remove()
+  vm.$destroy()
 }
 
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon: 'setting'
+    }
+  })
+  //设置间谍函数
+  let spy = chai.spy(function (){})
+  vm.$mount()
+  vm.$on('click',spy)
+  //希望这个函数被执行
+  vm.$el.click()
+  //期待间谍函数被调用了
+  expect(spy).to.have.been.called()
+}
