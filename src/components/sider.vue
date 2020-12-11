6921ef6d3a7f8c0ba85403139464c01bfcde1483
gitgundam <335968838@qq.com>
2020-12-11 15:42:47 +0800

    完成布局动画

--- src/components/sider.vue
+++ src/components/sider.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="show">
+      <slot></slot>
+      <button @click="isShow">close</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-  name: 'H-Sider'
-}
+  name: 'H-Sider',
+  data(){
+    return{
+      show: true
+    }
+  },
+  methods:{
+    isShow(){
+      this.show = false
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
+.sider {
+  width: 200px;
+  position: relative;
 
+  button {
+    position: absolute;
+    top: 4px;
+    right: 10px;
+  }
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: all .3s;
+}
+.slide-enter, .slide-leave-to  {
+  margin-left: -200px;
+}
 
 </style>
